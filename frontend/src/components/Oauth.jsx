6cd90afd8cd592cd54f6cb95e39a6e4ee91c36fd
RianFarhan07 <rian.mallanti@gmail.com>
2024-12-14 18:58:16 +0800

    google signin signup functionallity

--- frontend/src/components/Oauth.jsx
+++ frontend/src/components/Oauth.jsx
@@ -1,17 +1,39 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React from "react";
 import { FaGoogle } from "react-icons/fa";
-
-const handleGoogleSignUp = () => {
-  try {
-    console.log("Google Sign-up clicked");
-  } catch (error) {
-    console.log(error.message || "Google" + error);
-
-    setMessage("Google sign-up failde. please try again.");
-  }
-};
+import { auth } from "../firebase/firebase.config";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Oauth = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleGoogleSignUp = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+
+      const result = await signInWithPopup(auth, provider);
+
+      const response = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+
+      const data = await response.json();
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      console.log(error.message || "Google" + error);
+    }
+  };
   return (
     <div className="relative">
       <FaGoogle className="absolute left-4 top-1/2 -translate-y-1/2 transform text-white" />
